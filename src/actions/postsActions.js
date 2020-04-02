674ddde57fc172a09a46aaf2b05f5ffb60ba1778
dawneraq <dawneraq@gmail.com>
2020-04-02 15:14:45 -0700

    add postsActions and implement reducers for each action

---
+++ src/actions/postsActions.js
@@ -0,0 +1,36 @@
+// Create Redux action types
+export const GET_POSTS = 'GET POSTS';
+export const GET_POSTS_SUCCESS = 'GET POSTS_SUCCESS';
+export const GET_POSTS_FAILURE = 'GET POSTS_FAILURE';
+
+// Create Redux action creators
+export const getPosts = () => ({
+  type: GET_POSTS
+});
+
+export const getPostsSuccess = posts => ({
+  type: GET_POSTS_SUCCESS,
+  payload: posts
+});
+
+export const getPostsFailure = () => ({
+  type: GET_POSTS_FAILURE
+});
+
+// Combine action creators in async thunk
+export function fetchPosts() {
+  return async dispatch => {
+    dispatch(getPosts());
+
+    try {
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/posts'
+      );
+      const data = response.json();
+
+      dispatch(getPostsSuccess(data));
+    } catch (error) {
+      dispatch(getPostsFailure());
+    }
+  };
+}
